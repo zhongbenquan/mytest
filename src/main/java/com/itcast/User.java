9c6c4291e99323ff33a893c432aa6849eeabd56b
WQ <1136278412@qq.com>
2018-10-31 10:31:11 +0800

    第3次提交

--- src/main/java/com/itcast/User.java
+++ src/main/java/com/itcast/User.java
@@ -3,6 +3,6 @@ package com.itcast;
 public class User {
     public static void main(String[] args) {
         System.out.println("=======111111111111111========");
-        System.out.println("-----22222222-------");
+        System.out.println("-----222222222-------");
     }
 }
