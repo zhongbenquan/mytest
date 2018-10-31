7ec2939e5b63afde6e2531e79e07f06486815109
WQ <1136278412@qq.com>
2018-10-31 09:32:05 +0800

    第二次提交

--- src/main/java/com/itcast/User.java
+++ src/main/java/com/itcast/User.java
@@ -4,6 +4,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("=======111111111111111========");
         System.out.println("-----22222222-------");
-        System.out.println("-----333333333---------");
+
     }
 }
